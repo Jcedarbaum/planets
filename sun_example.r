143f77b6db28c94dcd310cb596d694772b491acb
Jcedarbaum <Jcedarbaum@gmail.com>
2017-04-24 18:08:30 -0700

    ....and it's a new label

--- sun_example.r
+++ sun_example.r
@@ -2,6 +2,6 @@
 sunlight = read.table("sun.txt", header=T)
 par(mar=c(5,6,3,2))
 boxplot(sunlight$Kdown_direct~sunlight$month,
-  ylab="Downwelling Solar\n kj/m2/day",
+  ylab="This is a new label",
   xlab="month", col="red")
 
